10ad80a104422ececdca63c1e00ffad6bed02849
gurpreet singh <gurpreetsingh@gurpreets-MacBook-Pro.local>
2020-10-29 16:23:48 +0530

    feat: add webpack eslint prettier husky docs: add instruction to setup

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container } from 'reactstrap';
 
 import './Main.css';
@@ -12,10 +7,10 @@ import Sidebar from '../sidebar/Sidebar';
 import Header from '../header/Header';
 import MainContent from './MainContent';
 
-const Main = props => {
+const Main = () => {
   return (
     <Router>
-      <Container className="themed-container" fluid={true}>
+      <Container className="themed-container" fluid>
         <Header />
         <Sidebar />
         <Switch>
@@ -26,10 +21,6 @@ const Main = props => {
       </Container>
     </Router>
   );
-}
-
-Main.propTypes = {
-  name: PropTypes.string
 };
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ const { registerRender } = window;
 function domRenderer() {
   ReactDOM.render(<App />, document.getElementById('root'));
 }
-// Function to execute when application is launch in Mercury application.
-// We are passing the root component which will render first in Merucy app.
-registerRender('child-app-one', { component: { app: MainContent }, domRenderer });
+// Function to execute when application is launch in parent application.
+// We are passing the root component which will render first in parent app.
+registerRender('child-app-one', {
+  component: { app: MainContent },
+  domRenderer,
+});
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
